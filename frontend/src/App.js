242be76383714d1505e9de3ee10c948baabdbd4c
Altaf <altafpatel8485@gmail.com>
2024-09-22 19:06:31 +0530

    setup react and tailwind for frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+// import logo from './logo.svg';
+import './App.css';
+import './main.css'
+
+function App() {
+  return (
+   <h1 className='text-red-500 capitalize'>hello jee</h1>
+  );
+}
+
+export default App;
